d7b898354cf30d36c01cb97667fa2b99547b406f
0xff <0xff@0xff.local>
2016-12-19 17:37:01 +0800

    first commit on web page

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-/* show only photo link if debug = 0*/
-debug = 1;
-fs = require('fs');
+'use strict'
 
 /* regullar expressions for parsing */
 const DCARD_HOST = "https://www.dcard.tw";
 const REG_DCARD_IMAGE = /(https?:\/\/imgur\.dcard\.tw\/\S+\.jpg)/g;
 const REG_DCARD_PAGE = /<a class="PostEntry_entry_2rsgm" href="(\S+)"\s+\S+>/g;
+var debug = 0;
 
-/* works like a UNIX cat, but it cat a URL */
-function cat(URL, callback)
+var src = "https://www.dcard.tw/f/sex?latest=true";
+
+function cat(theUrl, callback)
 {
-	var request = require('request')
-	URL = encodeURI(URL);
-
-	request({url:URL, method:'GET'}, function(e, r, b){
-		if(callback)
-			callback(b);
-	})
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous
+    xmlHttp.send(null);
 }
 
-/* works just like UNIX grep */
 function grep(source, format, callback){
 	if(!source){
 		return;
 	}
-	res = source.match(format);
+	var res = source.match(format);
 	if(res == null){
 		return -1;
 	}
@@ -36,52 +35,50 @@ function grep(source, format, callback){
 
 
 
-/*
-   what scroll(url) does :
-   1. cat content of archive page
-   2. grep post links(and save it to .page_link.txt)
-   3. cat the content of post
-   4. grep image link
-   5. grep photo links and write to .photo_link.txt
-   6. send request to get more content(i.e. to scroll down), and go to 1.
-*/
-
-
 /* Dcard use "last post sent id" as postfix in request header. It sends responses in JSON*/
 function scroll(LAST_ID){
-	next = "https://www.dcard.tw/_api/forums/sex/posts?popular=false&before=" + LAST_ID;
+	var next = "https://www.dcard.tw/_api/forums/sex/posts?popular=false&before=" + LAST_ID;
 	cat(next, function(content){
-		res = JSON.parse(content);
+		var res = JSON.parse(content);
 		if(debug)console.log(res);
 		for(var i = 0; i < res.length; i++){
-			link = "https://www.dcard.tw/f/sex/p/" + res[i].id;
+			var link = "https://www.dcard.tw/f/sex/p/" + res[i].id;
 			if(debug)console.log(link);
-			fs.writeFileSync(".dcard_pages", link, {flag:'a+'});
+			//fs.writeFileSync(".dcard_pages", link, {flag:'a+'});
 			cat(link, function(content){
 				grep(content, REG_DCARD_IMAGE, function(link){
-					console.log(link)
-					fs.writeFileSync(".photo_link.txt", link + "\n", {flag:'a+'});
+          console.log(link);
+          //alert(link);
+          var img = new Image();
+          var div = document.getElementById("Dcard-Sex-Pics");
+
+          img.onload = function() {
+            div.appendChild(img);
+          };
+
+          img.src = link;
+
+
 				})
 			})
 		}
-		scroll(res[res.length - 1].id);
+		//scroll(res[res.length - 1].id);
 	})
 }
 
 
 /* However, first request is HTML, which should be handled seperately */
 function view_menu(url){
-	fs = require('fs');
-	LAST_ID = "";
+	var LAST_ID = "";
     cat(url, function(content){
 		grep(content, REG_DCARD_PAGE, function(link){
 			link = DCARD_HOST + link;
-			fs.writeFileSync(".dcard_pages", link, {flag:'a+'});
+			//fs.writeFileSync(".dcard_pages", link, {flag:'a+'});
 			if(debug)console.log(link);
 			cat(link, function(content){
 				grep(content, REG_DCARD_IMAGE, function(link){
 					console.log(link);
-					fs.writeFileSync(".photo_link.txt", link + "\n", {flag:'a+'});
+					//fs.writeFileSync(".photo_link.txt", link + "\n", {flag:'a+'});
 				})
 			})
 			LAST_ID = link.replace(/.*\//g, "");
@@ -89,10 +86,10 @@ function view_menu(url){
 		if(debug)console.log(LAST_ID);
 		scroll(LAST_ID);
 	})
-	
+
 }
 
 
 /* the only code that will be executed for parsing is this */
-fs.writeFileSync(".photo_link.txt", "www.dcard.com" + "\n", {flag:'a+'});
+//fs.writeFileSync(".photo_link.txt", "www.dcard.com" + "\n", {flag:'a+'});
 view_menu("https://www.dcard.tw/f/sex?latest=true");
